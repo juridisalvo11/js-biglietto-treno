c46c96584a2a0cdb2bcd11ab8e307066c22abf80
Juri Di salvo <Juridisalvo11@gmail.com>
2020-04-20 18:01:39 +0200

    Js biglietto treno - personalizzazioni da aggiungere

--- sheet.js
+++ sheet.js
@@ -1,18 +1,28 @@
 //inserire kilometri da percorrere
-var km_passeggero = prompt('Kilmetri da percorrere');
-console.log(km_passeggero + 'km');
+var km_passeggero = prompt('Kil0metri da percorrere');
+//console.log(km_passeggero + 'km');
 //inserire anni passegero
 var anni_passeggero = prompt('Anni passeggero');
-console.log(anni_passeggero);
+//console.log(anni_passeggero);
 //calcolare prezzo totale viaggio
-var prezzo_kilometri = km_passeggero * 2;
+var prezzo_kilometri = km_passeggero * 0.21;
 //applicare sconto
 if (anni_passeggero < 18) {
-    console.log((prezzo_kilometri) - (prezzo_kilometri / 100) * 20);
-    console.log(prezzo_kilometri * 0.8);
+    var prezzo_kilometri = prezzo_kilometri * 0.8;
+    //console.log((prezzo_kilometri) - (prezzo_kilometri / 100) * 20);
+    //console.log(prezzo_kilometri * 0.8);
 } else if (anni_passeggero > 65) {
-    console.log((prezzo_kilometri) - (prezzo_kilometri / 100) * 40);
-    console.log(prezzo_kilometri * 0.6);
+    var prezzo_kilometri = prezzo_kilometri * 0.6;
+    //console.log((prezzo_kilometri) - (prezzo_kilometri / 100) * 40);
+    //console.log(prezzo_kilometri * 0.6);
 } else {
-    console.log(prezzo_kilometri);
+    //console.log(prezzo_kilometri);
 }
+
+document.getElementById('km-viaggio').innerHTML = km_passeggero + ' Km'
+document.getElementById('anni-passeggero').innerHTML = anni_passeggero
+document.getElementById('prezzo-biglietto').innerHTML = prezzo_kilometri + '€'
+
+document.getElementById('kilometri').setAttribute('class', 'visible');
+document.getElementById('eta').setAttribute('class', 'visible');
+document.getElementById('prezzo').setAttribute('class', 'visible');
